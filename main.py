6cd2d2275e55fab1afee35613ff2190f7d29eb71
KruzhokProg <Aa1@3$5^7>
2021-09-10 11:19:55 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+def smallestDifference(arrayOne, arrayTwo):
+    arrayOne.sort()
+    arrayTwo.sort()
+    idxOne = 0
+    idxTwo = 0
+    smallest = int("inf")
+    current = int("inf")
+    smallestPair = []
+    while idxOne < len(arrayOne) and idxTwo < len(arrayTwo):
+        firstNum = arrayOne[idxOne]
+        secondNum = arrayTwo[idxTwo]
+        if firstNum < secondNum:
+            current = secondNum - firstNum
+            idxOne += 1
+        elif secondNum < firstNum:
+            current = firstNum - secondNum
+            idxTwo += 1
+        else:
+            return [firstNum, secondNum]
+
+        if smallest > current:
+            smallest = current
+            smallestPair = [firstNum, secondNum]
+
+    return smallestPair
